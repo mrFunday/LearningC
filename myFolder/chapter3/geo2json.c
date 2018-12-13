43dc607cbabf2498536ddf52213d4fc68c38f8f3
mrFunday <44996784+mrFunday@users.noreply.github.com>
2018-12-13 13:39:24 +0300

    add spooky and bermuda

--- myFolder/chapter3/geo2json.c
+++ myFolder/chapter3/geo2json.c
@@ -11,7 +11,7 @@ int main()
 
 	puts("data=[");
 
-	while(scanf("%f,%f,%79[^\n]",&latitude,&longitude,info) == 3)
+	while(scanf("%f;%f;%79[^\n]",&latitude,&longitude,info) == 3)
 	{
 		if (started)
 		{
@@ -21,6 +21,15 @@ int main()
 		{
 			started = 1;
 		}
+
+		if (latitude < -90.0 || latitude > 90.0) {
+      		fprintf(stderr, "Invalid latitude: %f\n", latitude);
+      		return 2;
+    	}
+    	if (longitude < -180.0 || longitude > 180.0) {
+      		fprintf(stderr, "Invalid longitude: %f\n", longitude);
+      		return 2;
+    	}
 		printf("{latitude: %f, longitude: %f, info: '%s'}", latitude,longitude,info);
 	}
 	
